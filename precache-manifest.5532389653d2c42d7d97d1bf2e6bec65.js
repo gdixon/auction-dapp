83d8142566a3289790aeb263a139c87b4ec82af0
Graham Dixon <gdixon@assetinfo.co.uk>
2020-11-12 23:53:54 +0000

    connects to hosted substrate node

---
+++ precache-manifest.5532389653d2c42d7d97d1bf2e6bec65.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7394583803a5c5ba656406055ae1acea",
+    "url": "/auction-dapp/index.html"
+  },
+  {
+    "revision": "e12e5d41ae1843ccc245",
+    "url": "/auction-dapp/static/css/2.7858effa.chunk.css"
+  },
+  {
+    "revision": "e12e5d41ae1843ccc245",
+    "url": "/auction-dapp/static/js/2.d049f757.chunk.js"
+  },
+  {
+    "revision": "f9baea58926422c9ca74d98ae42cdbab",
+    "url": "/auction-dapp/static/js/2.d049f757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03fee0172ba80177d13",
+    "url": "/auction-dapp/static/js/main.67921a8b.chunk.js"
+  },
+  {
+    "revision": "bf35d58d238fcc5b9da1",
+    "url": "/auction-dapp/static/js/runtime-main.da2cba49.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/auction-dapp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/auction-dapp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/auction-dapp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/auction-dapp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/auction-dapp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/auction-dapp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/auction-dapp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/auction-dapp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/auction-dapp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/auction-dapp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/auction-dapp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/auction-dapp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/auction-dapp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/auction-dapp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/auction-dapp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/auction-dapp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
